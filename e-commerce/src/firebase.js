4f6e3396b84a359af6969560a961c9307e230ab8
kkimberly702 <bimby8701@gmail.com>
2022-08-22 19:16:10 -0700

    Firebase i

---
+++ e-commerce/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    
+    authDomain: "e-commerce-6f74c.firebaseapp.com",
+    projectId: "e-commerce-6f74c",
+    storageBucket: "e-commerce-6f74c.appspot.com",
+    messagingSenderId: "880797433338",
+    
+  };
